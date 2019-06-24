7724a054c09818a8bbf0304cf1d81d1f8f47fad8
Mateus Henrique <mateushenriquechaves1@gmail.com>
2019-06-24 17:59:56 -0300

    Enhancement resolved #8

--- UNE/MainWindow.xaml.cs
+++ UNE/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Data;
+using Microsoft.Win32;
 
 namespace UNE
 {
@@ -82,50 +83,62 @@ namespace UNE
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            var connString = "Server=localhost;Database=une-carteirinhas;Uid=root;Pwd=";
-            var connection = new MySqlConnection(connString);
-            var command = connection.CreateCommand();
-
-            Random R = new Random();
-            string code;
-            do
+            if (e.Source.ToString().Contains("Salvar"))
             {
-                code = ((long)R.Next(0, 100000) * (long)R.Next(0, 100000)).ToString().PadLeft(10, '0');
-            } while (codes.Contains(Convert.ToInt64(code)));
-            
-            try
-            {
-                connection.Open();
-                command.CommandText = "INSERT INTO `carteiras` (`name`, `cpf`, `rg`, `birthday`, `institution`, `image`, `course`, `coursetype`, `code`) VALUES ( @name, @cpf, @rg, @birthday, @institution, @image, @course, @coursetype, @code);";
-                command.Parameters.Add("@name", MySqlDbType.VarChar).Value= txtName.Text.Trim();
-                command.Parameters.Add("@cpf", MySqlDbType.VarChar).Value= txtCpf.Text.Trim();
-                command.Parameters.Add("@rg", MySqlDbType.VarChar).Value = txtRg.Text.Trim();
-                command.Parameters.Add("@birthday", MySqlDbType.VarChar).Value = dtpDate.Text;
-                command.Parameters.Add("@institution", MySqlDbType.VarChar).Value = txtInstitution.Text.Trim();
-                command.Parameters.Add("@image", MySqlDbType.VarChar).Value = "eu ts";
-                command.Parameters.Add("@course", MySqlDbType.VarChar).Value = txtCourse.Text.Trim();
-                command.Parameters.Add("@coursetype", MySqlDbType.VarChar).Value = cmbCourse.Text;
-                command.Parameters.Add("@code", MySqlDbType.VarChar).Value = code;
-                command.ExecuteNonQuery();
+                var connString = "Server=localhost;Database=une-carteirinhas;Uid=root;Pwd=";
+                var connection = new MySqlConnection(connString);
+                var command = connection.CreateCommand();
 
-                if (this.loadRegisters() == 0)
+                Random R = new Random();
+                string code;
+                do
                 {
-                    dataGrid.Opacity = 0;
-                    noData.Opacity = 100;
-                }
-                else
+                    code = ((long)R.Next(0, 100000) * (long)R.Next(0, 100000)).ToString().PadLeft(10, '0');
+                } while (codes.Contains(Convert.ToInt64(code)));
+
+                try
                 {
-                    noData.Opacity = 0;
-                    dataGrid.Opacity = 100;
+                    connection.Open();
+                    command.CommandText = "INSERT INTO `carteiras` (`name`, `cpf`, `rg`, `birthday`, `institution`, `image`, `course`, `coursetype`, `code`) VALUES ( @name, @cpf, @rg, @birthday, @institution, @image, @course, @coursetype, @code);";
+                    command.Parameters.Add("@name", MySqlDbType.VarChar).Value = txtName.Text.Trim();
+                    command.Parameters.Add("@cpf", MySqlDbType.VarChar).Value = txtCpf.Text.Trim();
+                    command.Parameters.Add("@rg", MySqlDbType.VarChar).Value = txtRg.Text.Trim();
+                    command.Parameters.Add("@birthday", MySqlDbType.VarChar).Value = dtpDate.Text;
+                    command.Parameters.Add("@institution", MySqlDbType.VarChar).Value = txtInstitution.Text.Trim();
+                    command.Parameters.Add("@image", MySqlDbType.VarChar).Value = "eu ts";
+                    command.Parameters.Add("@course", MySqlDbType.VarChar).Value = txtCourse.Text.Trim();
+                    command.Parameters.Add("@coursetype", MySqlDbType.VarChar).Value = cmbCourse.Text;
+                    command.Parameters.Add("@code", MySqlDbType.VarChar).Value = code;
+                    command.ExecuteNonQuery();
+
+                    if (this.loadRegisters() == 0)
+                    {
+                        dataGrid.Opacity = 0;
+                        noData.Opacity = 100;
+                    }
+                    else
+                    {
+                        noData.Opacity = 0;
+                        dataGrid.Opacity = 100;
+                    }
+
                 }
+                finally
+                {
+                    if (connection.State == ConnectionState.Open)
+                        connection.Close();
+                }
+            }else if (e.Source.ToString().Contains("Arquivo"))
+            {
+                var openFileDialog = new OpenFileDialog();
+                var dialogResult = openFileDialog.ShowDialog();
+                if (!dialogResult.HasValue) MessageBox.Show("Porra bixo, num fode !");
+                image.Source = new BitmapImage(new Uri(openFileDialog.FileName, UriKind.Absolute));
+                image.Width = 100;
+                image.Height = 100;
+
 
             }
-            finally
-            {
-                if (connection.State == ConnectionState.Open)
-                    connection.Close();
-            }
-
         }
 
 
